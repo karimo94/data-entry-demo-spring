0f7f7ed3d1522f6a5936fbaf6b34064a01d364fb
karimo94 <karimo94@outlook.com>
2025-08-27 01:50:49 -0400

    fixed repo interface

--- simpledataentry/src/main/java/com/karimo94/simpledataentry/repository/EmployeeRepo.java
+++ simpledataentry/src/main/java/com/karimo94/simpledataentry/repository/EmployeeRepo.java
@@ -6,6 +6,6 @@ import org.springframework.stereotype.Repository;
 import com.karimo94.simpledataentry.model.Employee;
 
 @Repository
-public interface EmployeeRepo extends JpaRepository<Employee, String>{
+public interface EmployeeRepo extends JpaRepository<Employee, Integer>{
     //custom query methods definitions here if need be
 }
