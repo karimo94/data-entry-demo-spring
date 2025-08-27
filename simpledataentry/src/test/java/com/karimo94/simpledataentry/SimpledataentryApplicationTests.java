e174ca9549d2e8e92e8a4489e33e2fcc38213910
karimo94 <karimo94@outlook.com>
2025-08-27 12:53:18 -0400

    added unit tests

--- simpledataentry/src/test/java/com/karimo94/simpledataentry/SimpledataentryApplicationTests.java
+++ simpledataentry/src/test/java/com/karimo94/simpledataentry/SimpledataentryApplicationTests.java
@@ -1,49 +1,74 @@
 package com.karimo94.simpledataentry;
 
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
 
+import java.util.List;
+import java.util.Set;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import com.karimo94.simpledataentry.controller.DataEntryController;
+import com.karimo94.simpledataentry.model.Employee;
+import com.karimo94.simpledataentry.repository.EmployeeRepo;
+import com.karimo94.simpledataentry.service.EmployeeService;
+
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.*;
 
 @SpringBootTest
+@ExtendWith(MockitoExtension.class)
 class SimpledataentryApplicationTests {
 
-	@Autowired
-	DataEntryController myTestController;
+	@Mock
+	private EmployeeRepo employeeRepo;
+	@InjectMocks
+	private EmployeeService employeeService;
+
+	Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
 
 	//should really use Mockito for these tests, but this is just a demo app
 	@Test
 	void testSaveEmployeeMissingFields() {
-		/*Employee testEmployee = new Employee();
-		testEmployee.age = 35;
-		testEmployee.hometown = "New York";
-		myTestController.savEmployee(testEmployee);	
-		List<Employee> allEmployees = myTestController.getAllEmployees();
-		assertEquals(allEmployees.size(), 0);*/
+		
+		Employee testEmployee = new Employee();
+		testEmployee.setAge(35);
+		testEmployee.setHometown("New York");
+		Employee response = employeeService.saveEmployee(testEmployee);
+		Set<ConstraintViolation<Employee>> violations = validator.validate(testEmployee);
+		assert(response == null);
+		assert(!violations.isEmpty());
 	}
 	@Test
 	void testSaveEmployee() {
-		/*Employee testEmployee = new Employee();
-		testEmployee.age = 35;
-		testEmployee.hometown = "New York";
-		myTestController.savEmployee(testEmployee);	
-		List<Employee> allEmployees = myTestController.getAllEmployees();
-		assertEquals(allEmployees.size(), 1);*/
+		Employee testEmployee = new Employee();
+		testEmployee.setAge(35);
+		testEmployee.setHometown("New York");
+		testEmployee.setName("Test User");
+		testEmployee.setTitle("Firmware Engineer");
+		given(employeeRepo.save(testEmployee)).willReturn(testEmployee);
+		Employee response = employeeService.saveEmployee(testEmployee);
+		assert(response != null && response.getName() == "Test User");
+		verify(employeeRepo).save(testEmployee);
 	}
 	@Test
 	void testGetAllEmployees() {
-		/*Employee emp1 = new Employee("Test User", 35, "Firmware Engineer", "New York, NY");
+		Employee emp1 = new Employee("Test User", 35, "Firmware Engineer", "New York, NY");
 		Employee emp2 = new Employee();
 		Employee emp3 = new Employee("Indiana Jones", 40, "Archaeologist", "Chicago, IL");
 		Employee emp4 = new Employee("",50,"Unknown", "Madrid, ES");
-		myTestController.savEmployee(emp1);
-		myTestController.savEmployee(emp2);
-		myTestController.savEmployee(emp3);
-		myTestController.savEmployee(emp4);
-		List<Employee> allEmployees = myTestController.getAllEmployees();
-		assertEquals(allEmployees, 2);*/
+		employeeService.saveEmployee(emp1);
+		employeeService.saveEmployee(emp2);
+		employeeService.saveEmployee(emp3);
+		employeeService.saveEmployee(emp4);
+		given(employeeRepo.findAll()).willReturn(List.of(emp1, emp3));
+		List<Employee> allEmployees = employeeService.getAllEmployees();
+		assert(allEmployees.size() == 2);
+		verify(employeeRepo).findAll();
 	}
 }
